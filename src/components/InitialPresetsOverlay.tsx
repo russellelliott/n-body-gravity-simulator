f0ba46824de63cc83696065a76f4c22a6657b21e
Russell Elliott <russell@quadsquad.net>
2026-07-25 09:42:07 -0700

    empty canvas preset allowed to be selected

--- src/components/InitialPresetsOverlay.tsx
+++ src/components/InitialPresetsOverlay.tsx
@@ -42,7 +42,7 @@ export const InitialPresetsOverlay: React.FC<InitialPresetsOverlayProps> = ({
 
         {/* Preset Cards Grid */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-          {PRESETS.filter(p => p.id !== 'empty').map((p) => (
+           {PRESETS.map((p) => (
             <button
               key={p.id}
               onClick={() => onSelectPreset(p)}
